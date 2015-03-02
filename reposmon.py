8cc400b7d7a31fa3033a123b507584d4c6658141
Erik de Jonge <erik@a8.nl>
2015-03-02 12:54:43 +0100

    reposmon Monday 02 March 2015 (week:9 day:61), 12:54:41

--- reposmon.py
+++ reposmon.py
@@ -1,73 +1,140 @@
 # coding=utf-8
 """
-reposmon
+reposmon.py:
+Monitor a git repository, execute a command when it changes.
 
 Usage:
     reposmon <giturl> <command>
-    [-g <gitdirectory>|--git-directory=<gitdirectory>]
-    [-c <commanddirectory>|--command-directory=<commanddirectory>]
+    [-i <interval>|--check-interval=<interval>]
+    [-g <gitfolder>|--git-folder=<gitfolder>]
+    [-c <commandfolder>|--command-folder=<commandfolder>]
 
 Options:
-  -h --help                               Show this screen.
-  -i --check-interval                     Seconds between checks [default: 10].
-  -g --git-directory=<gitdirectory>          Directory to check the git repos out [default: .].
-  -c --command-directory=<commanddirectory>  Directory from where to run the command [default: .].
+  -h --help                           Show this screen.
+  -i --check-interval=<interval>      Seconds between checks [default: 10].
+  -g --git-folder=<gitfolder>         Folder to check the git repos out [default: ~/workspace/reposmon].
+  -c --command-folder=<commandfolder> Folder from where to run the command [default: ~/].
 """
 
 # coding=utf-8
+from git import Repo
 from os.path import join, expanduser, exists, basename, expanduser
 from docopt import docopt
-from schema import Schema, SchemaError, Or, Optional, And
-from git import Repo
+from schema import Schema, SchemaError, Or, Optional, And, Use
+from subprocess import call
 
 
-def print_dictionary(argdict):
+def dictionary_for_console(argdict, indent=""):
+    """
+    @type argdict: dict
+
+    @type indent: str
+    @return: sp
+    """
     keys = argdict.keys()
     keys.sort(key=lambda x: len(x))
     sp = ""
     lk = 0
     ls = []
+
     for k in keys:
-        s = "\033[31m" + k + "\033[0m"
-        s += "\033[32m" + "`:" + argdict[k] + "\033[0m\n"
+        s = indent + "\033[32m" + k + "\033[0m"
+        s += "\033[33m" + "`: " + str(argdict[k]) + "\033[0m\n"
         ls.append((len(k), s))
-        
+
         if len(k) > lk:
             lk = len(k)
+
     for lns, s in ls:
         s = s.replace("`", " " * (1 + (lk - lns)))
         sp += s
-    print sp
+
+    return sp
 
 
-def print_arguments(arguments):
+def sort_arguments(arguments):
+    """
+    @type arguments: dict
+    @return: tuple
+    """
+    opts = {}
+    posarg = {}
+
+    for k in arguments:
+        if k.startswith("pa_"):
+            posarg[k.replace("pa_", "")] = arguments[k]
+
+        if k.startswith("op_"):
+            opts[k.replace("op_", "")] = arguments[k]
+
+    return opts, posarg
+
+
+def arguments_for_console(arguments):
     """
     @type arguments: dict
     @return: None
     """
-    options = {}
-    arguments = {}
-    for k in arguments:
-        if k.startswith("")
-    print_dictionary(arguments)
+    s = ""
+    opts, posarg = sort_arguments(arguments)
+    newline = False
+
+    if posarg:
+        s += "\033[31mPositional arguments:\033[0m\n"
+        s += dictionary_for_console(posarg, "  ")
+        newline = True
+
+    if opts:
+        if newline:
+            s += "\n"
+
+        s += "\033[31mOptions:\033[0m\n"
+        s += dictionary_for_console(opts, "  ")
+
+    return s.strip() + "\n"
 
 
-def parse_docopt():
+def raise_or_exit(e, debug=False):
+    """
+    @type e: Exception
+    @return: None
+    """
+    if debug:
+        raise e
+
+        exit(1)
+    else:
+        exit(1)
+
+
+def get_arguments(debug=False):
     """
     parse_docopt
     """
     arguments = dict(docopt(__doc__, version='0.1'))
+    try:
+        for k in arguments:
+            if "folder" in k or "path" in k:
+                if hasattr(arguments[k], "replace"):
+                    arguments[k] = arguments[k].replace("~", expanduser("~"))
 
-    for k in arguments:
-        if "directory" in k or "path" in k:
-            arguments[k] = arguments[k].replace("~", expanduser("~"))
+    except AttributeError as e:
+        print "\033[31mAttribute error:" + k.strip(), "->", str(e), "\033[0m"
+        print "\033[30m", "attrs: " + "\033[0m",
+
+        for k in arguments:
+            print "\033[30m", k.strip() + "\033[0m",
+
+        print
+        raise_or_exit(e, debug)
+
     try:
         schema = Schema({"<command>": str,
                          "<giturl>": lambda x: ".git" in x,
                          Optional("-i"): int,
-                         Optional("--check-interval"): int,
-                         Optional("--git-directory"): And(str, exists, error='--git-directory should exist'),
-                         Optional("--command-directory"): And(str, exists, error='--command-directory should exist')})
+                         Optional("--check-interval"): And(Use(int), error="[-i|--check-interval] must be an int"),
+                         Optional("--git-folder"): And(str, exists, error='[-g|--git-folder] should exist'),
+                         Optional("--command-folder"): And(str, exists, error='[-c|--command-folder] should exist')})
 
         arguments = schema.validate(arguments)
         arguments = dict((x.replace("<", "pa_").replace(">", "").replace("--", "op_").replace("-", "_"), y) for x, y in arguments.viewitems())
@@ -77,11 +144,15 @@ def parse_docopt():
         else:
             err = str(e)
 
-        print "\033[31m" + err, "\033[0m"
-        exit(1)
+        print "\033[31m" + err.strip() + "\033[0m"
+        print __doc__
+        raise_or_exit(e, debug)
 
-    print_arguments(arguments)
-    return arguments
+    if debug:
+        print arguments_for_console(arguments)
+
+    opts, posarg = sort_arguments(arguments)
+    return posarg, opts
 
 
 def clone_or_pull_from(remote, name):
@@ -133,7 +204,7 @@ def main():
     main
     git@github.com:erikdejonge/schema.git
     """
-    arguments = parse_docopt()
+    posarg, opts = get_arguments(True)
 
     # check_repos(arguments["giturl"])
 
