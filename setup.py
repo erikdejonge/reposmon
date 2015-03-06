01999a3ce8907df172d7957fc176f554f067ddef
Erik de Jonge <erik@a8.nl>
2015-03-06 16:57:53 +0100

    appinstance Friday 06 March 2015 (week:9 day:65), 16:57:50

--- setup.py
+++ setup.py
@@ -6,12 +6,7 @@ Active8 (04-03-15)
 author: erik@a8.nl
 license: GNU-GPL2
 """
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-from future import standard_library
-standard_library.install_aliases()
+
 from setuptools import setup
 setup(name='reposmon',
       version='16',
