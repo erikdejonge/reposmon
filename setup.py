f466e391ba44e2c5065b1bdf2b158474073b358f
Erik de Jonge <erik@a8.nl>
2015-03-06 16:28:29 +0100

    reposmon Friday 06 March 2015 (week:9 day:65), 16:28:27

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from future import standard_library
 standard_library.install_aliases()
 from setuptools import setup
 setup(name='reposmon',
-      version='15',
+      version='16',
       description='Monitor a git repository, execute a command when it changes. Basically a polling git-hook for pull.',
       url='https://github.com/erikdejonge/reposmon',
       author='Erik de Jonge',
