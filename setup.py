4118fd4cb3b7fce70a65b20ae1abda25d58b40a0
Erik de Jonge <erik@a8.nl>
2015-03-06 12:34:54 +0100

    pip Friday 06 March 2015 (week:9 day:65), 12:34:51

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ license: GNU-GPL2
 """
 from setuptools import setup
 setup(name='reposmon',
-      version='8',
+      version='9',
       description='Monitor a git repository, execute a command when it changes. Basically a polling git-hook for pull.',
       url='https://github.com/erikdejonge/reposmon',
       author='Erik de Jonge',
